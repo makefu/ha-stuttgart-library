8cb580aeefd23f674f6aa2c05d73e7b3241fedc6
makefu <github@syntax-fehler.de>
2024-10-12 22:18:26 +0200

    flake.nix: add poetry, pre-commit

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
         # Use this shell for developing your app.
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.myapp ];
-          nativeBuildInputs = with pkgs.python3Packages; [
+          nativeBuildInputs = with pkgs; [
             pre-commit poetry
           ];
           
